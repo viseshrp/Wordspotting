b8fc541b26dce6948846e4dcb04a3fe186405512
viseshrp <viseshrprasad@gmail.com>
2017-12-21 18:19:23 -0500

    add background/event script

---
+++ js/eventPage.js
@@ -0,0 +1,9 @@
+/**
+A background page:
+Runs in the extension process.
+Holds main logic of the extension.
+Exists for the lifetime of the extension.
+Only triggered based on events as opposed to persistent bg pages. (persistent:false)
+Handles state and state changes of UI elements.
+*/
+
