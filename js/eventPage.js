3136c09a1693323ec50f20139090a467ee7e5a93
viseshrp <viseshrprasad@gmail.com>
2017-12-22 15:24:09 -0500

    Add message passing and handling

--- js/eventPage.js
+++ js/eventPage.js
@@ -1,9 +1,27 @@
 /**
-A background page:
-Runs in the extension process.
-Holds main logic of the extension.
-Exists for the lifetime of the extension.
-Only triggered based on events as opposed to persistent bg pages. (persistent:false)
-Handles state and state changes of UI elements.
-*/
+ A background page:
+ Runs in the extension process.
+ Holds main logic of the extension.
+ Exists for the lifetime of the extension.
+ Only triggered based on events as opposed to persistent bg pages. (persistent:false)
+ Handles state and state changes of UI elements.
+ */
 
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+
+        if (request.wordfound !== null) {
+
+            sendResponse({
+                ack: "gotcha", tab_url: sender.tab ?
+                    "from a content script:" + sender.tab.url :
+                    "from the extension"
+            });
+            //responds synchronously, after everything is done
+
+            if (request.wordfound === true) {
+                //fire notification.
+            }
+
+        }
+    });
--- js/mainContentScript.js
+++ js/mainContentScript.js
@@ -1,20 +1,24 @@
 //global variables
+
 //fire event
+
+chrome.runtime.sendMessage({wordfound: isWordFound()}, function (response) {
+    console.log(response.tab_url);
+    console.log("contentscript ack: " + response.ack);
+});
 
 // other functions
 
 function isWordFound() {
     var isFound = false;
-    var keyword_list = ['Citizenship|citizen','clearance', 'security clearance'];
+    var keyword_list = ['Citizenship|citizen', 'clearance', 'security clearance'];
     //todo get it from options page. //might have to use message passing to get it.
 
     for (var key in keyword_list) {
 
         var word = keyword_list[key];
-        console.log("for word : " + word);
 
         // $("*:contains('" + word.toLowerCase() + "')")
-
         //get list of elements matching the word or regex,
         //whatever is in the key list.
         var filtered_elements = $("body").filter(function () {
