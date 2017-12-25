a64b45ee1404fc0508951aeb094b20e223c92e28
viseshrp <viseshrprasad@gmail.com>
2017-12-25 17:20:19 -0500

    Open options page properly

--- js/popup.js
+++ js/popup.js
@@ -8,4 +8,15 @@ $(document).ready(function () {
             });
         }
     });
+
+
+    $("#options_btn").click(function () {
+        if (chrome.runtime.openOptionsPage) {
+            // New way to open options pages, if supported (Chrome 42+).
+            chrome.runtime.openOptionsPage();
+        } else {
+            // Reasonable fallback.
+            window.open(chrome.runtime.getURL('options.html'));
+        }
+    });
 });
