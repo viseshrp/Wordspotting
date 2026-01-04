47ef37252e064b5b9744c3d758bc279f8065ae73
Visesh Rajendraprasad <viseshrprasad@gmail.com>
2026-01-04 12:16:28 -0500

    Set up Jest with jsdom and shared chrome mocks

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "js/utils.js",
+    "js/settings.js",
+    "js/content.js"
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+      statements: 90,
+      functions: 90,
+      branches: 80
+    }
+  }
+};
---
+++ tests/setup.js
@@ -0,0 +1,43 @@
+// Jest setup: provide global mocks for Chrome APIs and browser-y globals.
+
+global.chrome = {
+  runtime: {
+    lastError: null,
+    onInstalled: { addListener: jest.fn() },
+    onMessage: { addListener: jest.fn() },
+    sendMessage: jest.fn()
+  },
+  storage: {
+    sync: {
+      set: jest.fn((obj, cb) => cb && cb()),
+      get: jest.fn((keys, cb) => cb && cb({}))
+    }
+  },
+  permissions: {
+    contains: jest.fn((_, cb) => cb && cb(true)),
+    request: jest.fn((_, cb) => cb && cb(true))
+  },
+  action: {
+    setBadgeText: jest.fn()
+  },
+  notifications: {
+    create: jest.fn()
+  },
+  tabs: {
+    create: jest.fn(),
+    query: jest.fn()
+  },
+  scripting: {
+    executeScript: jest.fn(),
+    insertCSS: jest.fn()
+  }
+};
+
+global.importScripts = () => {};
+global.requestIdleCallback = (cb) => cb();
+global.cancelIdleCallback = () => {};
+global.MutationObserver = class {
+  constructor(callback) { this.cb = callback; }
+  observe() {}
+  disconnect() {}
+};
