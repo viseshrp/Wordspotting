23520f4d1e5c83c8e7bd9ec7293b0a69ebfe2734
Visesh Rajendraprasad <viseshrp@users.noreply.github.com>
2026-02-20 20:06:28 -0500

    add a manifest key (#26)

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,9 +5,12 @@ import { defineConfig } from 'wxt';
 const pkg = JSON.parse(readFileSync(resolve(__dirname, 'package.json'), 'utf8')) as {
   version: string;
 };
+const MANIFEST_KEY =
+  'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxTXma4csNnYitWbhLXF3YvLIIs+I0ILdscV2sz4FP8MMIJFaeC7Jkx4jpPhqfV5HqQhpinrYxtopjFSsc280YdNbNwG2DK8MVVzjrn5Rf5SE9vlFg7qPJ24PGyef72ptZCitt9owS/4xx7SQHspxmLT74Mbx29ecb5opRJhRn++Cl/E8UZDS4H47RJIU3NzroUpfpWwZxWkZmscp4dGWUfqwMGftMHiztnPytk6ccPp14vg0Gj+HnXaSYsx/gvwnRUqMbH8FcxS4u6lpg36367ZajKps4dosRhsod0VC0XFlDskPm9eImYuGPrtMNASpKrS2fbEU5zs69limEhqgIwIDAQAB';
 
 export default defineConfig({
   manifest: {
+    key: MANIFEST_KEY,
     name: 'Wordspotting',
     description: 'Get notified when a word is found in a web page.',
     version: pkg.version,
